b4062b1dbbfe709283896eea62e02125ff94a82c
Eric Wang <109598120+Ricwan4@users.noreply.github.com>
2025-09-17 00:25:25 +0800

    Delete Formal_Code directory

--- Formal_Code/HMM-MAR-master/episodic/m/updateSigma_ehmm.m
+++
@@ -1,4 +0,0 @@
-function ehmm = updateSigma_ehmm(ehmm,rangeK)
-if nargin < 2 || isempty(rangeK), rangeK = 1:ehmm.K; end % K+1
-ehmm = updateSigma(ehmm,rangeK);
-end
--- Formal_Code/HMM-MAR-master/utils/plot/plot4paper.m
+++
@@ -1,9 +0,0 @@
-function plot4paper(xtext,ytext)
-
-a=axis;
-if(nargin<2)ytext='';end;
-set(gca,'fontsize',16);
-set(gca,'LineWidth',2);
-xlabel(xtext,'fontsize',16);
-ylabel(ytext,'fontsize',16);
-axis(a);
